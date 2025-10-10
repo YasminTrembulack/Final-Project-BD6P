7c8699ec699bca13d66960236e4c7fbe8fb2fc9d
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-10-10 17:17:58 -0300

    feat: update config to get env vars

--- config.py
+++ config.py
@@ -1,7 +1,15 @@
-db_config ={
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'root',
-    'database': 'final_project_db',
-    'port': 5433 #3306
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_NAME = os.getenv("DB_NAME")
+
+db_config = {
+    "host": os.getenv("DB_HOST", "root"),
+    "user": os.getenv("DB_USER", "root"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": DB_NAME,
+    "port": int(os.getenv("DB_PORT", 3306))
 }
