9b42f6ee2434cc27f0b78398be6f39b6bfcdaded
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-10-13 21:15:28 -0300

    feat: remove gitkeep and add users routes

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import secrets
 
 from flask import Flask
 
+from controllers import main_controller
+
 # Gustavo de Souza
 # Israel Victor
 # Maria Eduarda Selhorst
@@ -9,6 +11,9 @@ from flask import Flask
 
 app = Flask(__name__, template_folder="./views/templates", static_folder="./views/static")
 app.secret_key = secrets.token_hex(32)
+
+
+main_controller.configure_routes(app)
 
 
 if __name__ == "__main__":
