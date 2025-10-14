bd7e92b1b0a6148a79926fc74761a11565cc182f
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-10-13 21:14:00 -0300

    chore: remove DB_NAME const

--- config.py
+++ config.py
@@ -4,12 +4,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_NAME = os.getenv("DB_NAME")
 
 db_config = {
     "host": os.getenv("DB_HOST", "root"),
     "user": os.getenv("DB_USER", "root"),
     "password": os.getenv("DB_PASSWORD"),
-    "database": DB_NAME,
+    "database": os.getenv("DB_NAME"),
     "port": int(os.getenv("DB_PORT", 3306))
 }
