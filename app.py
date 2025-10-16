f950619e1d13e79f7dbb3cbcb27a5de85fc424ff
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-10-16 12:36:10 -0300

    feat: add controller and routes to app

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import secrets
 
-from flask import Flask
+from flask import Flask, render_template
 
-from controllers import main_controller
+from controllers import auth_controller
+from controllers import user_controller
 
 # Gustavo de Souza
 # Israel Victor
@@ -13,7 +14,13 @@ app = Flask(__name__, template_folder="./views/templates", static_folder="./view
 app.secret_key = secrets.token_hex(32)
 
 
-main_controller.configure_routes(app)
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+auth_controller.configure_routes(app)
+user_controller.configure_routes(app)
 
 
 if __name__ == "__main__":
